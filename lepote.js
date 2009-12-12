64729e659bf93075842a02973740f798da621e7c
Thomas Debarochez <thomas.barochez+github@gmail.com>
2009-12-12 14:17:20 +0100

    remove loadPlugins function from library and move it to bot file. add following listeners : - "resources.binded" (called when client is ready to chat), - "message.sent" - "presence.receive" (when receive status informations about a contact)

--- lepote.js
+++ lepote.js
@@ -1,4 +1,6 @@
-var xmpp = require('./lib/xmpp');
+var xmpp = require('./lib/xmpp'),
+   posix = require('posix'),
+     sys = require('sys');
 
 try {
   var lepote = new xmpp.Client({login: 'test',
@@ -8,6 +10,26 @@ try {
 				port: 5222,
 				resource: 'LePote',
 				status: 'Au rapport, Sir !'});
+  lepote.addListener('resources.binded', loadPlugins);
+
 } catch (e) {
   process.stdio.writeError(e + "\n");
+}
+
+function loadPlugins () {
+  posix.readdir('./plugins/').addListener('success', function (files) {
+    for (var k = 0; k < files.length; ++k) {
+      if (!(/\.js$/.exec(files[k]))) {
+        continue;
+      }
+      var filename = './plugins/' + files[k];
+      var plugin = require(filename.replace(/(\.js)$/, ''));
+      for (var j = 0; j < plugin.listeners.length; ++j) {
+	lepote.addListener(plugin.listeners[j].event, plugin.listeners[j].func);
+      }
+      sys.puts('[ load ] ' + filename + ' ' + plugin.listeners.length + ' listeners');
+    }
+  }).addListener('error', function () {
+    sys.puts('[ warn ]  unable to load plugin directory');
+  });
 }
---
+++ plugins/admin.js
@@ -0,0 +1,22 @@
+var sys = require('sys');
+this.listeners = [];
+this.listeners.push({'event': 'message.receive',
+		     'func' : function(from, content, to, type, id) {
+                       if (/^ping$/.exec(content)) {
+			 this.push(from, 'pong');
+                       } else {
+                         this.push(from, 'command unknow : ' + content);
+                       }
+                     }});
+this.listeners.push({'event': 'presence.receive',
+		     'func' : function(from, to, status, priority) {
+		       this.push('admin@localhost',
+				 'receive presence from : ' + from
+			         + ' to : ' + to
+				 + ' status : ' + status
+				 + ' priority : ' + priority);
+                     }});
+this.listeners.push({'event': 'message.sent',
+		     'func' : function(to, msg) {
+		       sys.puts('[ send ] ' + to + ': ' + msg);
+                     }});
