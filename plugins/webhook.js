eeff5023c5500de11083016fa0ada24fd4b12b65
Thomas Debarochez <thomas.barochez+github@gmail.com>
2010-06-30 23:40:18 +0200

    add a github post-receive hooks plugin read http://help.github.com/post-receive-hooks/ for more informations

---
+++ plugins/webhook.js
@@ -0,0 +1,26 @@
+var http = require('http'),
+    sys = require('sys'),
+    querystring = require('querystring');
+
+var that;
+http.createServer(function(request, response) {
+  var body = '';
+  request.addListener('data', function(chunk) {
+    body = body + chunk;
+  });
+  request.addListener('end', function() {
+    var params = JSON.parse(querystring.parse(body).payload);
+    response.writeHead(200, {"content-length":"message",
+			     "content-type":"text/plain"});
+    response.end('thx !');
+    var authors = [];
+    params.commits.forEach(function (commit) {
+      if (authors.join().indexOf(commit.author.name) == -1) {
+        authors.push(commit.author.name);
+      }
+    });
+    that.push('thomas.barochez@gmail.com', params.commits.length + ' commits has just been pushed on ' + params.repository.name + ' repository by : ' + authors.join(', '));
+  });
+}).listen(8123);
+
+exports.events = [function() { that = this; }];
