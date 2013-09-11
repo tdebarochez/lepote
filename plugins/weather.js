ab677dce34e79be8eb75b783d24d83588c7c4dcd
Thomas Debarochez <thomas.barochez+github@gmail.com>
2013-09-11 22:57:51 +0200

    google weather api discontinued, replace by wunderground.com

--- plugins/weather.js
+++ plugins/weather.js
@@ -2,28 +2,33 @@ var http = require('http');
 
 lepote.on('message', function(from, content) {
   if (content.substr(0, 4) === 'help') {
-    return this.push(from, 'weather <place> : give you the current weather state at <place>.');
+    return this.push(from, 'weather <city>, <country> : give you the current weather state at <city>.');
   }
   var that = this;
-  var res = /^weather\s(.+)$/.exec(content);
+  var res = /^weather\s([^,]+),\s+(.*)$/.exec(content);
   if (res === null) {
     return;
   }
+  //http://api.wunderground.com/api/1e124db77eef6c84/geolookup/conditions/q/FR/Paris.json
   var options = {
-    host: 'www.google.com',
+    host: 'api.wunderground.com',
     port: 80,
     method: 'GET',
-    path: "/ig/api?"+require('querystring').stringify({"weather": res[1], "hl": 'en'})
+    path: "/api/1e124db77eef6c84/geolookup/conditions/q/" + res[2] + '/' + res[1] + '.json'
   };
   var request = http.get(options, function (response) {
+    if (response.statusCode !== 200) {
+      that.push(from, res[1] + ' not found');
+      return;
+    }
     response.setEncoding("utf8");
     var buffer = '';
     response.on("data", function (chunk) {
       buffer += chunk;
     });
     response.on("end", function () {
-      var condition = /\<current_conditions\s*\>.*\<condition\s+data="([^"]+)"\s*\/\>.*\<temp_c\s+data="([^"]+)"\s*\/\>.*\<humidity\s+data="([^"]+)"\s*\/\>.*\<\/current_conditions\>/.exec(buffer);
-      that.push(from, condition[1] + ', ' + condition[2] + 'C, ' + condition[3]);
+      var current = JSON.parse(buffer).current_observation;
+      that.push(from, res[1] + ' : ' + current.temperature_string + ', humidity : ' + current.relative_humidity);
     });
   });
   request.end();
